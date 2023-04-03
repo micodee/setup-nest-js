aa1bb45f64fca6123d3dd14af38d390156e59087
micodee <martomhidayat>
2023-04-03 19:53:22 +0700

    add provider

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get} from '@nestjs/common';
+import { BooksService } from './books.service';
 
 @Controller('books')
 export class BooksController {
-  @Get('/hello')
-  hello() {
-    return 'Hello World';
+  private booksService: BooksService
+
+  constructor(booksService : BooksService) {
+   this.booksService = booksService;
+  }
+  
+  @Get()
+  getAllBooks() {
+   return this.booksService.getAllBooks()
   }
 }
